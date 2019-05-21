12812e2d9c06949c065065dc164b7f633cc4a991
Paulo Medeiros <pauloricardodemedeiros@hotmail.com>
2019-05-20 22:03:56 -0300

    check if a feature is aa GeometryCollection

--- index.js
+++ index.js
@@ -14,56 +14,73 @@
         output[idx++] = input[i];
       }
     }
-    return (Array.isArray(output[0])) ? output : [output];
-  };
+    return Array.isArray(output[0]) ? output : [output];
+  }
 
   function maxLat(coords) {
-    return Math.max.apply(null, coords.map(function(d) { return d[1]; }));
+    return Math.max.apply(
+      null,
+      coords.map(function(d) {
+        return d[1];
+      })
+    );
   }
 
   function maxLng(coords) {
-    return Math.max.apply(null, coords.map(function(d) { return d[0]; }));
+    return Math.max.apply(
+      null,
+      coords.map(function(d) {
+        return d[0];
+      })
+    );
   }
 
   function minLat(coords) {
-    return Math.min.apply(null, coords.map(function(d) { return d[1]; }));
+    return Math.min.apply(
+      null,
+      coords.map(function(d) {
+        return d[1];
+      })
+    );
   }
 
   function minLng(coords) {
-    return Math.min.apply(null, coords.map(function(d) { return d[0]; }));
+    return Math.min.apply(
+      null,
+      coords.map(function(d) {
+        return d[0];
+      })
+    );
   }
 
   function fetchEnvelope(coords) {
     var mmc = {
-      "minLng": minLng(coords),
-      "minLat": minLat(coords),
-      "maxLng": maxLng(coords),
-      "maxLat": maxLat(coords)
-    }
+      minLng: minLng(coords),
+      minLat: minLat(coords),
+      maxLng: maxLng(coords),
+      maxLat: maxLat(coords)
+    };
 
     return {
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": [[
-          [mmc.minLng, mmc.minLat],
-          [mmc.minLng, mmc.maxLat],
-          [mmc.maxLng, mmc.maxLat],
-          [mmc.maxLng, mmc.minLat],
-          [mmc.minLng, mmc.minLat]
-        ]]
+      type: "Feature",
+      properties: {},
+      geometry: {
+        type: "Polygon",
+        coordinates: [
+          [
+            [mmc.minLng, mmc.minLat],
+            [mmc.minLng, mmc.maxLat],
+            [mmc.maxLng, mmc.maxLat],
+            [mmc.maxLng, mmc.minLat],
+            [mmc.minLng, mmc.minLat]
+          ]
+        ]
       }
-    }
+    };
   }
 
   function fetchExtent(coords) {
-    return [
-      minLng(coords),
-      minLat(coords),
-      maxLng(coords),
-      maxLat(coords)
-    ]
+    return [minLng(coords), minLat(coords), maxLng(coords), maxLat(coords)];
   }
 
   // Adapted from http://stackoverflow.com/questions/2792443/finding-the-centroid-of-a-polygon
@@ -71,8 +88,8 @@
     var centroid = {
       x: 0,
       y: 0
-    }
-    
+    };
+
     var signedArea = 0;
     var x0 = 0;
     var y0 = 0;
@@ -85,7 +102,7 @@
       y0 = vertices[i][1];
       x1 = vertices[i + 1][0];
       y1 = vertices[i + 1][1];
-      a = (x0 * y1) - (x1 * y0);
+      a = x0 * y1 - x1 * y0;
 
       signedArea += a;
       centroid.x += (x0 + x1) * a;
@@ -96,19 +113,22 @@
     y0 = vertices[vertices.length - 1][1];
     x1 = vertices[0][0];
     y1 = vertices[0][1];
-    a = (x0 * y1) - (x1 * y0);
+    a = x0 * y1 - x1 * y0;
     signedArea += a;
     centroid.x += (x0 + x1) * a;
     centroid.y += (y0 + y1) * a;
 
     signedArea = signedArea * 0.5;
-    centroid.x = centroid.x / (6.0*signedArea);
-    centroid.y = centroid.y / (6.0*signedArea);
+    centroid.x = centroid.x / (6.0 * signedArea);
+    centroid.y = centroid.y / (6.0 * signedArea);
 
     return [centroid.x, centroid.y];
   }
 
   function feature(obj) {
+    if (obj.geometry.type === "GeometryCollection") {
+      return flatten(obj.geometry.geometries.map(process));
+    }
     return flatten(obj.geometry.coordinates);
   }
 
@@ -170,13 +190,12 @@
   }
 
   module.exports = {
-    "envelope": envelope,
-    "extent": extent,
-    "centroid": centroid,
-    "xMin": xMin,
-    "xMax": xMax,
-    "yMin": yMin,
-    "yMax": yMax
-  }
-
-}());
+    envelope: envelope,
+    extent: extent,
+    centroid: centroid,
+    xMin: xMin,
+    xMax: xMax,
+    yMin: yMin,
+    yMax: yMax
+  };
+})();
